Deal with RFNWithSTNet model bug, adding batch normalization on the last layer

models/net_model.py
@@ -134,14 +134,19 @@ class RFNWithSTNet(torch.nn.Module):
         super(RFNWithSTNet, self).__init__()
         # Initial convolution layers
         self.conv1 = ConvLayer(3, 32, kernel_size=5, stride=2)
+        self.bn1 = torch.nn.BatchNorm2d(num_features=32)
         self.conv2 = ConvLayer(32, 64, kernel_size=3, stride=2)
+        self.bn2 = torch.nn.BatchNorm2d(num_features=64)
         self.conv3 = ConvLayer(64, 128, kernel_size=3, stride=1)
+        self.bn3 = torch.nn.BatchNorm2d(num_features=128)
         self.resid1 = ResidualBlock(128)
         self.resid2 = ResidualBlock(128)
         self.resid3 = ResidualBlock(128)
         self.resid4 = ResidualBlock(128)
         self.conv4 = ConvLayer(128, 64, kernel_size=3, stride=1)
+        self.bn4 = torch.nn.BatchNorm2d(num_features=64)
         self.conv5 = ConvLayer(64, 1, kernel_size=1, stride=1)
+        self.bn5 = torch.nn.BatchNorm2d(num_features=1)
         # output shape: [bs, 1, 32, 32]
 
         # Spatial Transformer Network
@@ -158,8 +163,6 @@ class RFNWithSTNet(torch.nn.Module):
             nn.ReLU(True),
             nn.Linear(32, 3 * 2)
         )
-        self.fc_loc[2].weight.data.zero_()
-        self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
 
         for m in self.modules():
             if isinstance(m, torch.nn.Conv2d):
@@ -173,26 +176,29 @@ class RFNWithSTNet(torch.nn.Module):
                 nn.init.normal_(m.weight, 0, 0.01)
                 nn.init.zeros_(m.bias)
 
+        self.fc_loc[2].weight.data.zero_()
+        self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+
     def stn(self, x):
         xs = self.localization(x)
         xs = xs.view(-1, 10 * 4 * 4)
         theta = self.fc_loc(xs)
         theta = theta.view(-1, 2, 3)
-        grid = F.affine_grid(theta, x.size())
-        x = F.grid_sample(x, grid)
+        grid = F.affine_grid(theta, x.size(), align_corners=True)
+        x = F.grid_sample(x, grid, align_corners=True)
 
         return x
 
     def forward(self, x):
-        conv1 = F.relu(self.conv1(x))
-        conv2 = F.relu(self.conv2(conv1))
-        conv3 = F.relu(self.conv3(conv2))
+        conv1 = F.relu(self.bn1(self.conv1(x)))
+        conv2 = F.relu(self.bn2(self.conv2(conv1)))
+        conv3 = F.relu(self.bn3(self.conv3(conv2)))
         resid1 = self.resid1(conv3)
         resid2 = self.resid1(resid1)
         resid3 = self.resid1(resid2)
         resid4 = self.resid1(resid3)
-        conv4 = F.relu(self.conv4(resid4))
-        conv5 = F.relu(self.conv5(conv4))
+        conv4 = F.relu(self.bn4(self.conv4(resid4)))
+        conv5 = F.relu(self.bn5(self.conv5(conv4)))
 
         out = self.stn(conv5)
 